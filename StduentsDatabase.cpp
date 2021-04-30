953425883eb25ddd81374279b9e31608a952df1d
okomojacob <jaylabs2030@gmail.com>
2021-04-30 16:41:06 +0300

    :books: :satellite: updates

---
+++ StduentsDatabase.cpp
@@ -0,0 +1,89 @@
+/*
+C++ program to create GEGIS Student database, read and print given number(Say n) student's details
+(Example of array of objects).
+A lot of commenting is to enhance understanding of anyone too green in programming to at lest have a grip
+*/
+#include <bits/stdc++.h> //For std iteration to keep on prompting the user to input and continuously save the entries
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+#define MAX 500  //Maximum number of/Limit of students
+
+class Student   //Start of OOP class instantiation
+{
+    private:   //Access specifier.How the variables will be accessed by outside/global operators.
+
+        string  name;
+        string  Reg_No;
+        float   total_Mrks;
+        float   grading_points;
+
+    public: //The details remain publicly accessible to all from outside access
+
+        //member function to get student's details
+        void getDetails();
+
+        void PrintTitle()
+       {
+           cout<<"\t\t\t GEGIS STUDENT DATABASE DEMO IN OOP"<<endl;
+           cout<<"\t\t\t ==================================\n"<<endl;
+       }
+
+        //Member function to out put student's details
+        void displayDetails(void);
+};// End of class instance.
+
+//Member function definition, outside of the class at end of class
+void Student::getDetails(void)
+{
+    cout << "Name: ";
+    cin >> name;
+    cout << "Student's Reg No: ";
+    cin >> Reg_No;
+    cout << "Total marks out of 100: ";
+    cin >> total_Mrks;
+
+    grading_points = (total_Mrks/100)*5;
+}
+
+//member function definition, outside of the class
+
+void Student::displayDetails(void)
+{
+    cout << "Student Details: "<<endl;
+    cout << "Name:"<< name << " | Reg Number:" << Reg_No << " | Total: " << total_Mrks << " | Percentage:" << grading_points<<"| Grade: "<<endl;
+}
+
+int main()
+{
+    Student std[MAX];       //Array of objects creation
+    int n, iter;            //n is the number of students, iter is any iterable from index 0 to index (n-1)
+    Student PT;             //PT in this case is a function that will invoke the printTitle() as shown exactly below.
+    PT.PrintTitle();
+
+    cout << "Enter total number of students: " ;
+    cin >> n;
+
+    for(iter = 0; iter < n; iter++) //We are now looping to keep on getting details from the 1st to the nth student.
+        {
+            cout <<endl<<"Enter details of student " << iter+1 <<endl;
+            std[iter].getDetails();
+        }
+
+    cout << endl;
+
+    for(iter=0;iter< n; iter++) //We are finally looping to keep on displaying the initially entered details from the 1st to the nth student.
+
+        {
+            cout <<endl<<"Details of student " << (iter+1) << ":\n";
+            std[iter].displayDetails(); //The displayDetails function displays the details.
+        }
+
+
+    return 0;
+
+}
+
+//End of code
